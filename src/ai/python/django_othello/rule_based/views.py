07d45e153b90da989e6f5245e230c5e7369b0deb
ryskchy <chr.de92lll1.zlk@gmail.com>
2018-06-25 23:08:39 +0900

    fix

--- src/ai/python/django_othello/rule_based/views.py
+++ src/ai/python/django_othello/rule_based/views.py
@@ -1,7 +1,3 @@
-from os import path
-import sys
-sys.path.append(path.dirname(path.dirname(
-    path.dirname(path.abspath(__file__)))))
 from django.shortcuts import render
 from django.http import JsonResponse
 import othello
@@ -9,4 +5,4 @@ import othello
 
 def left_top(request):
     # TODO 合法手の中から左上のマスを選択するロジックを実装
-    return JsonResponse({"cell": "4c", "color": "black"})
+    return JsonResponse({"cell": "c4", "color": "black"})
