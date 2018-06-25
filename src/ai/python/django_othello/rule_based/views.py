9c865dd4dc8683a88bb1151c4f3b3dbd0b709741
ryskchy <chr.de92lll1.zlk@gmail.com>
2018-06-25 22:56:46 +0900

    edit django files

--- src/ai/python/django_othello/rule_based/views.py
+++ src/ai/python/django_othello/rule_based/views.py
@@ -1,3 +1,12 @@
+from os import path
+import sys
+sys.path.append(path.dirname(path.dirname(
+    path.dirname(path.abspath(__file__)))))
 from django.shortcuts import render
+from django.http import JsonResponse
+import othello
 
-# Create your views here.
+
+def left_top(request):
+    # TODO 合法手の中から左上のマスを選択するロジックを実装
+    return JsonResponse({"cell": "4c", "color": "black"})
--- src/ai/python/othello.py
+++ src/ai/python/othello.py
@@ -8,6 +8,14 @@ N = 8
 COLUMNS = "abcdefgh"
 ROWS = "12345678"
 
+_nabor = 9
+
+# 上下左右斜めのインデックスを作る
+DIFFS = np.zeros([_nabor, 2], dtype=np.int8)
+DIFFS[:, 0] = np.arange(_nabor, dtype=np.int8) // 3 - 1
+DIFFS[:, 1] = np.arange(_nabor, dtype=np.int8) % 3 - 1
+DIFFS = DIFFS[np.arange(_nabor) != 4]
+
 
 class CellState(IntEnum):
     Empty = 0
@@ -42,6 +50,36 @@ def decode_board(sboard: str):
     return np.array([CHAR_TO_NUM[i] for i in sboard], dtype=np.int8).reshape(N, N)
 
 
-def check_flip_cells(board: np.ndarray, x, y, isListUp=False):
+def _check_flip_cells(board: np.ndarray, x: int, y: int, color: int, is_list_up: bool = False):
 
-    pass
+    if board[x, y] != 0:
+        return None
+    nabors = DIFFS + (x, y)  # 周辺8マスのインデックス配列
+    nabor_cells = board[nabors[:, 0], nabors[:, 1]]
+    is_flipable_nabor = nabor_cells == (3 - color)
+    if not is_list_up:
+        return is_flipable_nabor.any()
+    directions = DIFFS[is_flipable_nabor]  # 敵の石が置いてあるマスとの差分
+    flippable_indices = np.array([], dtype=np.int8).reshape(0, 2)
+    for dxy in directions:  # TODO for文使わない実装
+        count = 2
+        nxy = (x, y) + dxy * count
+        while((nxy >= 0) & (nxy < N)).all():
+            if board[nxy] == CellState.Empty:
+                break
+            if board[nxy] == color:
+                flippable_indices = np.r_[flippable_indices,
+                                          np.arange(1, count, dtype=np.int8)
+                                          [:, np.newaxis]*dxy]
+                break
+
+            count += 1
+    return flippable_indices
+
+
+def can_put_stone(board: np.ndarray, x: int, y: int, color: int):
+    return bool(_check_flip_cells(board, x, y, color, False))
+
+
+def list_flip_cells(board: np.ndarray, x: int, y: int, color: int):
+    return _check_flip_cells(board, x, y, color, True)
