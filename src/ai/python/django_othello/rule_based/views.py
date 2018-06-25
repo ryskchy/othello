9708455933314b79b15a30fb1c465f66f5f82fd2
ryskchy <ryskchy@users.noreply.github.com>
2018-06-25 23:39:13 +0900

    edit response mock (#4)

--- src/ai/python/django_othello/rule_based/views.py
+++ src/ai/python/django_othello/rule_based/views.py
@@ -5,4 +5,8 @@ from . import othello
 
 def left_top(request):
     # TODO 合法手の中から左上のマスを選択するロジックを実装
-    return JsonResponse({"cell": "c4", "color": "black"})
+    data = {"is_passed": False,
+            "cell": "c4",
+            "color": "black"
+            }
+    return JsonResponse(data)
